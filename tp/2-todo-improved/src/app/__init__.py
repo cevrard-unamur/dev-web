6d926ac07413244d52caa876eb1c8bcd0ae7848f
Cedric Evrard <cedric@evrard.io>
2020-04-11 22:47:30 +0200

    add email sending when register

--- tp/2-todo-improved/src/app/__init__.py
+++ tp/2-todo-improved/src/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+mail = Mail(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
--- tp/2-todo-improved/src/app/routes/login.py
+++ tp/2-todo-improved/src/app/routes/login.py
@@ -1,9 +1,11 @@
 from app import app
 from app import db
+from app import mail
 
 from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_required, login_user, logout_user, current_user
 from werkzeug.urls import url_parse
+from flask_mail import Message
 
 from app.models.task import Task
 from app.models.user import User
@@ -12,6 +14,7 @@ from app.forms.registrationForm import RegistrationForm
 from app.data.userDataAccess import UserDataAccess
 
 import logging
+import sys
 
 @app.route('/login', methods=['get', 'post'])
 def login():
@@ -51,6 +54,16 @@ def register():
                 form.lastname.data,
                 form.birthday.data,
                 form.email.data)
+            try:
+                send_welcome_mail(
+                    form.email.data,
+                    form.username.data,
+                    form.firstname.data,
+                    form.lastname.data
+                )
+            except:
+                e = sys.exc_info()[0]
+                logging.error(e)
             flash('Votre compte a été créé, vous pouvez maintenant vous connecter', 'success')
             return redirect(url_for('login'))
         else :
@@ -64,3 +77,28 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+def send_welcome_mail(to, username, firstname, lastname):
+    msg = Message("Bienvenue sur ToDo !",
+                sender="cedric@evrard.io",
+                recipients=[to])
+    msg.html = """
+            <html>
+                <head>
+                </head> 
+                <body>
+                    <h1>
+                        Bienvenue sur ToDo !
+                    </h1>
+                    Bonjour %s %s,
+                    <br>
+                    <br>
+                    <br>
+                    Votre nom d'utilisateur est <strong>%s</strong>.
+                    <br>
+                    Vous pouvez vous connecter au site via cette adresse : <a href="http://localhost:5000/login">http://localhost:5000/login</a> 
+                </body>
+            </html>
+        """ % (firstname, lastname, username.lower())
+
+    mail.send(msg)
